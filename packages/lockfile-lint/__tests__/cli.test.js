53d57bad844b6ba509dea46652a72bc2bd84f557
Liran Tal <liran.tal@gmail.com>
2020-02-08 21:04:50 +0200

    fix(cli): conflicting arguments will error

--- packages/lockfile-lint/__tests__/cli.test.js
+++ packages/lockfile-lint/__tests__/cli.test.js
@@ -50,10 +50,16 @@ describe('CLI tests', () => {
     })
 
     process.stdout.on('close', exit => {
-      expect(output.indexOf('detected invalid protocol for package: debug@^4.1.1\n    expected: https:\n    actual: http:\n')).not.toBe(
-        -1
-      )
-      expect(output.indexOf('detected invalid protocol for package: ms@^2.1.1\n    expected: https:\n    actual: http:\n')).not.toBe(-1)
+      expect(
+        output.indexOf(
+          'detected invalid protocol for package: debug@^4.1.1\n    expected: https:\n    actual: http:\n'
+        )
+      ).not.toBe(-1)
+      expect(
+        output.indexOf(
+          'detected invalid protocol for package: ms@^2.1.1\n    expected: https:\n    actual: http:\n'
+        )
+      ).not.toBe(-1)
       expect(output.indexOf('error: command failed with exit code 1')).not.toBe(-1)
       done()
     })
@@ -70,6 +76,28 @@ describe('CLI tests', () => {
 
     process.on('close', exitCode => {
       expect(exitCode).toBe(1)
+      done()
+    })
+  })
+
+  test('Providing conflicting arguments should display an error', done => {
+    const process = childProcess.spawn(cliExecPath, [
+      '--type',
+      'yarn',
+      '--path',
+      '__tests__/fixtures/yarn-only-http.lock',
+      '--validate-https',
+      '--allowed-schemes',
+      'https:'
+    ])
+
+    let output = ''
+    process.stderr.on('data', chunk => {
+      output += chunk
+    })
+
+    process.stderr.on('close', _ => {
+      expect(output.indexOf('Arguments o and validate-https are mutually exclusive')).not.toBe(-1)
       done()
     })
   })
